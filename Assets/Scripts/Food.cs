d2b6c37c7f06cdb230df2f178f3d34257f7ae785
Adam Brodin <worecraft@gmail.com>
2018-09-26 14:02:49 +0200

    Kaffe och Kanelbulle
    Adam & Bogdan SU17A

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Food : Item
+{
+    #region Variables
+    private bool alive;
+    SpriteRenderer spr;
+    #endregion
+
+    public void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    public void OnMouseUpAsButton()
+    {
+        if (survived() == true)
+        {
+            alive = true;
+            nextLevel();
+        }
+        else if (survived() == false)
+        {
+            alive = false;
+            die();
+        }
+    }
+
+    private void nextLevel()
+    {
+        StartCoroutine(timer());
+    }
+
+    private void die()
+    {
+        StartCoroutine(timer());
+        //Application.Quit();
+    }
+    private IEnumerator timer()
+    {
+        if (alive)
+        {
+            spr.color = Color.green;
+        }
+        else if (!alive)
+        {
+            spr.color = Color.red;
+        }
+        yield return new WaitForSeconds(0.5f);
+        spr.color = Color.white;
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private Color normalColor;
+    private Color highlightColor;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+        /* if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = highlightColor;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = normalColor;
+        }*/
+    }
+
+    public bool survived()
+    {
+        float i = Random.Range(1, 100);
+        Debug.Log(i);
+        if (i >= (100 * 1 / 6f))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
